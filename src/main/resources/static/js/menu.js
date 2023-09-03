a41f89612ddcf4421b93359a1d4083d5521f5bef
alejandraMedinaC <alejandramcampagna@gmail.com>
2023-09-03 16:55:37 -0400

    Creacion de condicional en pantalla menu

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,6 +1,7 @@
 const vm = new Vue({
     el: '#menu',
     data: {
+        status:"new"
     },
 
     created() {
@@ -8,14 +9,32 @@ const vm = new Vue({
     },
 
     computed: {
+        isProfile(){
+            return this.status === "profile";
+        }
     },
 
     methods: {
         singIn: function () {
             window.location = window.location.origin + "/login";
             setTimeot("redirect()", 1000);
+        },
+        goToProfile: function (){
+           this.status = "profile"
+        },
+        goToProjects: function (){
+           this.status = "project"
+        },
+        goToAdmin: function (){
+           this.status = "admin"
+        },
+        goToTasks: function (){
+           this.status = "task"
         }
+        
     }
+    
+    
 });
 
 
