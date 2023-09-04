1c7b87e3a6d9aac2b99683afb6b069f37de4c705
alejandraMedinaC <alejandramcampagna@gmail.com>
2023-09-03 21:24:15 -0400

    Profile screen

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,8 +1,19 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        status:"new",
-        n:1
+        status: "new",
+        user: {
+            id: undefined,
+            username: "",
+            password: "",
+            identification_number: undefined,
+            first_name: "",
+            last_name: "",
+            phone_number: "",
+            email: "",
+            date_of_birth: "",
+            status: ""
+        }
     },
 
     created() {
@@ -10,32 +21,58 @@ const vm = new Vue({
     },
 
     computed: {
-        isProfile(){
+        isProfile() {
             return this.status === "profile";
+        },
+        isProject() {
+            return this.status === "project";
+        },
+        isAdmin() {
+            return this.status === "admin";
+        },
+        isTask() {
+            return this.status === "task";
         }
     },
-
     methods: {
         singIn: function () {
             window.location = window.location.origin + "/login";
             setTimeot("redirect()", 1000);
         },
-        goToProfile: function (){
-           this.status = "profile";
+
+        goToProfile: function () {
+            this.status = "profile";
         },
-        goToProjects: function (){
-           this.status = "project";
+
+        goToProjects: function () {
+            this.status = "project";
         },
-        goToAdmin: function (){
-           this.status = "admin";
+
+        goToAdmin: function () {
+            this.status = "admin";
         },
-        goToTasks: function (){
-           this.status = "task";
+
+        goToTasks: function () {
+            this.status = "task";
+        },
+
+        searchUser: function () {
+            let service_path = window.location.origin + "/users/" + this.user.id;
+            console.log(service_path);
+
+            axios.get(service_path).then(rs => {
+                let {data} = rs;
+                console.log(data);
+//                const cambiada = fecha_nac.split("/").reverse().join("/");
+//                console.log(cambiada)
+                this.user = data;
+            }).catch(error => {
+                this.getNotify("Usuario y/o contraseña incorrecta.", "danger", "top", 50, "right", 500, 3000, true, 10);
+                console.log(error);
+            });
         }
-        
     }
-    
-    
+
 });
 
 
